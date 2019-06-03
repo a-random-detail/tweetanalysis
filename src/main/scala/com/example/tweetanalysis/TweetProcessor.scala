7e7c14ff5aef2034bb0bb6e45f013fda9da7e679
Dalton DeMaria <dalton.demaria@gmail.com>
2019-06-03 17:15:04 -0500

    too many streams for counting

--- src/main/scala/com/example/tweetanalysis/TweetProcessor.scala
+++ src/main/scala/com/example/tweetanalysis/TweetProcessor.scala
@@ -11,18 +11,23 @@ trait TweetProcessor[F[_]] {
 }
 
 case class AnalysisResult(totalTweets: Int, tweetsPerSecond: Double, tweetsPerMinute: Double, tweetsPerHour: Double, topHashtags: Map[Hashtag, Int])
+// case class AnalysisResult(totalTweets: Int,  topHashtags: Map[Hashtag, Int])
 object TweetProcessor {
   def impl[F[_]: Sync]: TweetProcessor[F] = new TweetProcessor[F] {
     val startTime = LocalTime.now()
     def analyze(s: Stream[F, Tweet]): Stream[F, AnalysisResult] = {
-      def count = s.scan(0)((acc,_) => acc+1).drop(1)
-      def perSecondStream = count.map(_.toDouble / timeDeltaSeconds)
+
+      def count = s.scan((0, 0.0))((acc,_) => {
+        val total = acc._1 + 1
+        (total, total.toDouble/timeDeltaSeconds)
+      }).drop(1)
+
       def topHashtags = s.map(_.entities.hashtags)
         .scan(Map[Hashtag, Int]())((acc, next) => acc.combine(next.groupBy(i=>i).mapValues(_.size)))
         .drop(1)
-      def tweetCounts = count.zipWith(perSecondStream)((a,b) => (a,b, b*60, b*60*60))
-      tweetCounts.zipWith(topHashtags)((a,b) => AnalysisResult(a._1, a._2, a._3, a._4, retrieveTopHashtags(b)))
+      count.zipWith(topHashtags)((a,b) => AnalysisResult(a._1,a._2, a._2*60, a._2*60*60, retrieveTopHashtags(b)))
     }
+    
     private def retrieveTopHashtags(m: Map[Hashtag, Int]): Map[Hashtag, Int] = m.toList.sortWith((a,b) => {
       if (a._2 == b._2) {
         a._1.text < b._1.text
